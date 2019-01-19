025131dc5b9f5d63279931c2d3b5ef6710c125dd
Денис Падрухин <contact@inediapps.com>
2019-01-19 20:53:01 +0300

    portfolio best transform animation

--- js/init.js
+++ js/init.js
@@ -226,27 +226,19 @@ function inedi_tm_portfolio(){
 //		var el			= jQuery(this);
 //		var el2			= el.find('.list_inner');
 //		var image		= el.find('.image_wrap');
-//        var definition = el.find('.definition_portfolio');
-
-     
-
-//		var definitionH	= el.find('.definition_portfolio').outerHeight();
-
-       
 		
 //			el2.on('mouseenter',function(){
-//				image.css({top:-definitionH/2});
-//				definition.css({marginTop:-definitionH});
+//				//image.css({top:-definitionH/2});
+//                image.css({margin:-30});
 //			}).on('mouseleave',function(){
-//				image.css({top:0});
-//				definition.css({marginTop:0});
+//				//image.css({top:0});
+//                image.css({margin:0});
 //			});
-           
-        
         
 //    });
 
 //}
+
 
 // -----------------------------------------------------
 // --------------------    TOTOP    --------------------

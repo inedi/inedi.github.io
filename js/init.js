39cd6158bc20ae111b3e45389a4a33b8a091842c
Денис Падрухин <contact@inediapps.com>
2019-02-03 01:26:17 +0300

    app - > apps

--- js/init.js
+++ js/init.js
@@ -357,7 +357,7 @@ function inedi_tm_animate_text() {
     var animateSpan = jQuery('.inedi_tm_animation_text_word');
 
     animateSpan.typed({
-        strings: ["UI DESIGNERS", "App DEVELOPMENT", "UX Engineering",  "UWP WPF XAML UNITY"],
+        strings: ["UI DESIGNERS", "Apps DEVELOPMENT", "UX Engineering",  "UWP WPF XAML UNITY"],
         loop: true,
         startDelay: 1e3,
         backDelay: 2e3
